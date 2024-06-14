944db8c0ba37893c1ae4ad98432e714c170e9005
Lenovo <xinkai.li@my.jcu.edu.au>
2024-06-14 18:22:47 +0800

    this is list_exercises.py

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,71 @@
+
+numbers = []
+for i in range(5):
+    number = float(input("Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+#cheak
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter your username: ")
+
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
+
+#List comprehensions
+"""
+CP1404/CP5632 Practical
+List comprehensions
+"""
+
+names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
+full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
+
+# for loop that creates a new list containing the first letter of each name
+first_initials = []
+for name in names:
+    first_initials.append(name[0])
+print(first_initials)
+
+# list comprehension that does the same thing as the loop above
+first_initials = [name[0] for name in names]
+print(first_initials)
+
+# list comprehension that creates a list containing the initials
+# this splits each name and adds the first letters of each part to a string
+full_initials = [name.split()[0][0] + name.split()[1][0] for name in full_names]
+print(full_initials)
+
+# this example uses filtering to select only the names that start with A
+a_names = [name for name in names if name.startswith('A')]
+print(a_names)
+
+# and here's the join string method being used to create a single string from the names like:
+# 'Ada Alan Angel Bob Jimi'
+print(" ".join(sorted(names)))
+
+# list comprehension to create a list of all the full_names in lowercase format
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
+
+almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
+# list comprehension to create a list of integers from the above list of strings
+numbers = [int(num) for num in almost_numbers]
+print(numbers)
+
+# list comprehension to create a list of only the numbers that are greater than 9 from the numbers (not strings) you just created
+greater_than_9 = [num for num in numbers if num > 9]
+print(greater_than_9)
+
+# (more advanced) use a list comprehension and the join string method to create a string (not list) of the last names for those full names longer than 11 characters
+long_last_names = ", ".join([name.split()[1] for name in full_names if len(name) > 11])
+print(long_last_names)
