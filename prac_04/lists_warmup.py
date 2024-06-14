312ddd40e05dfa767f52708b3e728821ce6c289b
Lenovo <xinkai.li@my.jcu.edu.au>
2024-06-14 18:10:18 +0800

    this is list_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,26 @@
+# List of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Predictions of the values of the following expressions:
+# numbers[0] -> 3
+# numbers[-1] -> 2
+# numbers[3] -> 1
+# numbers[:-1] -> [3, 1, 4, 1, 5, 9]
+# numbers[3:4] -> [1]
+# 5 in numbers -> True
+# 7 in numbers -> False
+# "3" in numbers -> False (because "3" is a string, not an integer)
+# numbers + [6, 5, 3] -> [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Perform the following actions on the list 'numbers'
+# Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+
+# Change the last element of numbers to 1
+numbers[-1] = 1
+
+# Print all the elements from numbers except the first two (slice)
+print(numbers[2:])
+
+# Print whether 9 is an element of numbers
+print(9 in numbers)
