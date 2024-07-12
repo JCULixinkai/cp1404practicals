cb55a66aa518d2fda90761c17d381c0633454bb2
Lenovo <xinkai.li@my.jcu.edu.au>
2024-07-12 15:06:15 +0800

    this is guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -12,25 +12,4 @@ print(f"{guitar2.name} get_age() - Expected 9. Got {guitar2.get_age()}")
 print(f"{guitar1.name} is_vintage() - Expected True. Got {guitar1.is_vintage()}")
 print(f"{guitar2.name} is_vintage() - Expected False. Got {guitar2.is_vintage()}")
 
-from prac_06.guitar import Guitar
 
-def main():
-    """Collect and display guitar information."""
-    guitars = []
-    print("My guitars!")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.")
-        name = input("Name: ")
-
-
-    print("\nThese are my guitars:")
-    for i, guitar in enumerate(guitars, 1):
-        vintage_string = " (vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-
-    main()
